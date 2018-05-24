d8aaf2cab2c37492bbe5c8095e97dba36014cf7f
gp <gp@catchbest.com>
2018-05-24 17:57:36 +0800

    add ar0130 130w, add aec functions, more param save for KSJ_ParamProgram

--- KSJApi.Inc/KSJApi3A.h
+++ KSJApi.Inc/KSJApi3A.h
@@ -73,24 +73,25 @@ extern "C"{
 	// 开始或终止聚焦评估
 	KSJ_API  int __stdcall KSJ_AFStart(int nChannel, bool bStart);
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// 硬件自动曝光
 	KSJ_API  int __stdcall KSJ_AECSetEnable ( int nChannel, bool bEnable  );
-	KSJ_API  int __stdcall KSJ_AECGetEnable ( int nChannel, bool *bEnable  );
+	KSJ_API  int __stdcall KSJ_AECGetEnable ( int nChannel, bool *pbEnable  );
 
 	KSJ_API  int __stdcall KSJ_AECSetRange ( int nChannel, unsigned short usMin,  unsigned short usMax  );
-	KSJ_API  int __stdcall KSJ_AECGetRange ( int nChannel, unsigned short *usMin, unsigned short *usMax );
+	KSJ_API  int __stdcall KSJ_AECGetRange ( int nChannel, unsigned short *pusMin, unsigned short *pusMax );
 	// 得到自动曝光调节的当前曝光值
-	KSJ_API  int __stdcall KSJ_AECGetCur ( int nChannel, unsigned short *usCurGain  );
+	KSJ_API  int __stdcall KSJ_AECGetCur ( int nChannel, unsigned short *usCur  );
 
 	// 硬件自动增益
 	KSJ_API  int __stdcall KSJ_AGCSetEnable ( int nChannel, bool bEnable  );
-	KSJ_API  int __stdcall KSJ_AGCGetEnable ( int nChannel, bool *bEnable  );
+	KSJ_API  int __stdcall KSJ_AGCGetEnable ( int nChannel, bool *pbEnable  );
 
 	KSJ_API  int __stdcall KSJ_AGCSetRange ( int nChannel, unsigned short usMin,  unsigned short usMax  );
-	KSJ_API  int __stdcall KSJ_AGCGetRange ( int nChannel, unsigned short *usMin, unsigned short *usMax );
+	KSJ_API  int __stdcall KSJ_AGCGetRange ( int nChannel, unsigned short *pusMin, unsigned short *pusMax );
 
 	// 得到自动增益调节的当前增益值
-	KSJ_API  int __stdcall KSJ_AGCGetCur ( int nChannel, unsigned short *usCurGain  );
+	KSJ_API  int __stdcall KSJ_AGCGetCur ( int nChannel, unsigned short *pusCur  );
 
 #ifdef __cplusplus
 }
